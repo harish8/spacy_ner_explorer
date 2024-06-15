a5865b8d72664a79e9dd4b1cdc4e8ce2f46f4e75
Harish <harishbabu.webmaster@gmail.com>
2024-06-15 04:07:17 -0400

    Updated README and grouping the list of categories and unique sections to analyze.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Named Entity Recognition (NER) project using SpaCy. NER is a task in NLP where w
 like names of people, organizations, locations, dates, and other important information. 
 This project will help you get familiar with SpaCy and its capabilities in handling text data.
 
+## Streamlit app link
+https://harish8-spacy-ner-explorer.streamlit.app
+
 ## Dataset source 
 
 `CNN News Articles from 2011 to 2022`
--- ner_explorer.py
+++ ner_explorer.py
@@ -7,6 +7,7 @@ Created on Mon May 27 15:35:43 2024
 """
 
 import pandas as pd
+import numpy as np
 import spacy
 import streamlit as st
 import spacy_streamlit
@@ -22,6 +23,14 @@ Named Entity Recognition (NER) project using SpaCy. NER is a task in NLP where w
 like names of people, organizations, locations, dates, and other important information. 
 This project will help you get familiar with SpaCy and its capabilities in handling text data.    '''
     )
+
+st.markdown('''
+            ## **`This dataset contains around 38000 lines of articles from CNN news from the year 2011 to 2022.`**
+
+            Source: https://www.kaggle.com/datasets/hadasu92/cnn-articles-after-basic-cleaning/data
+
+            ''')
+            
 #%%
 # load the articles dataset in a var `cnn_articles_df`
 cnn_articles_df = pd.read_csv('./data/CNN_Articels_clean.csv')
@@ -35,10 +44,45 @@ df_cnn_articles_info = buffer.getvalue()
 st.text(df_cnn_articles_info)
 # cnn_articles_df.info()
 
+
+cnn_articles_df.rename(columns = {
+    'Date Published' : 'DatePublished',
+    'Second headline' : 'SecondHeadline',
+    'Article text' : 'ArticleText'
+        })
+
+st.markdown('''
+            For this NLP project the dataset looks good with all the object types. 
+            I will change the column names with spaces to CamelCase to keep it ideal to work with like the following. 
+            `'Date Published' : 'DatePublished',
+            'Second headline' : 'SecondHeadline',
+            'Article text' : 'ArticleText'`
+            ''')
+            
 st.markdown(f" ## Get statistics of the dataset")
 st.write(cnn_articles_df.describe())
 
 #%%
-st.markdown(f" ## let's take a look of first few rows how the data is looking like.")
+st.markdown(f" ## let's take a look in first few rows how the data is looking like.")
 pd.set_option("display.max_columns",200)
 st.dataframe(cnn_articles_df.head())
+
+st.markdown('''
+            Looks neat so far. now we can start choosing some interesting features to explore and analyse the articles in the dataset.
+            ''')
+#%%
+# Choosing the intresting features to analyse in the dataset.
+st.markdown(f" ## Types of articles covered in sections under each category")
+group_cat_sections = cnn_articles_df.groupby(['Category']).agg({
+    'Section' : lambda x : list(x.unique())
+    }).reset_index()
+
+st.markdown(f" (Double click on the cell to see the full list) ")
+st.dataframe(group_cat_sections, use_container_width=True)
+
+#%%
+st.markdown('''
+            Pretty standard list of article sections under each category and pretty straight forward that `health` and `politics` are just as their name suggests.
+            \n 
+            Although there are many intresting sections to dive into I'm curious to dig deeper into `tech` under `business` category. Wonder how tech is evolved since 2011 to 2022. 
+            ''')
