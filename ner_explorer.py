89f729ae9208dbdb2c76c363352471447c1ad940
Harish <harishbabu.webmaster@gmail.com>
2024-07-24 21:59:36 -0400

    Fetching Tech articles and using visualizers highlighting ners

--- ner_explorer.py
+++ ner_explorer.py
@@ -12,6 +12,9 @@ import streamlit as st
 import spacy_streamlit
 import io
 
+from spacy_streamlit import load_model
+from spacy import displacy
+
 
 #%%
 st.title("SpaCy NER Explorer")
@@ -30,8 +33,18 @@ st.markdown('''
             ''')
             
 #%%
+            
+spacy_model = st.sidebar.selectbox("Model name", ["en_core_web_md", "en_core_web_sm"])
+nlp = load_model(spacy_model)
+
+
+            
+#%%
 # load the articles dataset in a var `cnn_articles_df`
 cnn_articles_df = pd.read_csv('./data/CNN_Articels_clean.csv').set_index('Index').reset_index(drop=True)
+
+
+# print(f"Unique word count: {len(set(' '.join(cnn_articles_df['Headline']).split()))}")
 
 #%%
 st.markdown(" ## Get summary of the dataset's structure")
@@ -42,12 +55,13 @@ df_cnn_articles_info = buffer.getvalue()
 st.text(df_cnn_articles_info)
 # cnn_articles_df.info()
 
+#%%
 
 cnn_articles_df.rename(columns = {
-    'Date Published' : 'DatePublished',
+    'Date published' : 'DatePublished',
     'Second headline' : 'SecondHeadline',
     'Article text' : 'ArticleText'
-        })
+        }, inplace=True)
 
 st.markdown('''
             For this NLP project the dataset looks good with all the object types. 
@@ -57,6 +71,7 @@ st.markdown('''
             'Article text' : 'ArticleText'`
             ''')
             
+
 st.markdown(" ## Get statistics of the dataset")
 st.write(cnn_articles_df.describe())
 
@@ -95,8 +110,8 @@ st.markdown('''
             Pretty standard list of article sections under each category and pretty straight forward 
             that `health` and `politics` are just as their name suggests.
             \n 
-            Although there are many intresting sections to dive into I'm curious to dig deeper 
-            into `tech` under `business` category. Wonder how tech is evolved since 2011 to 2022. 
+            Although there are many intresting sections to dive into, for now I'll fetch the articles in
+            `tech` under `business` category to explore the Named Entities. Wonder how tech is evolved since 2011 to 2022. 
             ''')
 
 
@@ -111,3 +126,34 @@ st.write(f" Total number of articles in tech = {len(cnn_articles_in_tech)} ")
 st.dataframe(cnn_articles_in_tech)
 
 #%%
+
+st.markdown('''
+            Here we will use the SpaCy built-in `displaCy visualizer` highlighting the entities and their labels in a text from the choosen article. 
+            ''')
+        
+        
+st.markdown('''
+            There will be two SpaCy pretrained models to choose on the left sidebar. 
+            `en_core_web_sm` is a small size model
+            `en_core_web_md` is a medium size model
+            
+            Feel free to play around choosing different models. 
+            ''')
+
+# Process the text with spaCy
+# create a dropdown selectbox with a list of headlines in tech. 
+
+option = st.selectbox("Choose an Article", cnn_articles_in_tech['Headline'])
+ArticleRecord = cnn_articles_in_tech[cnn_articles_in_tech['Headline']== option]
+
+doc = nlp(ArticleRecord.iloc[0]['ArticleText'])
+ent_html = displacy.render(doc, style="ent", jupyter=False)
+st.html(ent_html)
+
+#%%
+
+st.markdown('''
+            ------
+            I will conclude this project here since the main purpose of this project is to fetch the text and explore the Named Entity Recognition. 
+            We will dive deeper into NLP with BERT, SpaCy and NLTK in the future projects which is already in progress. 
+            ''')
